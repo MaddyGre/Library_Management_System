cd9f170d741c50cfae19557dcb9f676b8f54e644
MaddyGre <maddalenaonyehara@gmail.com>
2020-09-20 20:43:12 +0100

    Data now displays when row is selected but still unable to update on the database

--- Library_Management_System/src/com/maddy/Admin.java
+++ Library_Management_System/src/com/maddy/Admin.java
@@ -6,6 +6,7 @@ import com.mysql.jdbc.Statement;
 
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -20,6 +21,7 @@ import javax.swing.JButton;
 
 import java.awt.Font;
 
+import javax.swing.AbstractButton;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -30,22 +32,18 @@ import javax.swing.JScrollPane;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 
-
-
-
-
-
-
-
-
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Vector;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ListSelectionModel;
 
 public class Admin extends JFrame {
 
@@ -54,13 +52,68 @@ public class Admin extends JFrame {
 	private JTextField author_TextField;	
 	private JScrollPane scrollPane;
 	DefaultTableModel model;
-	private JTable table;
+	//private JTable table;
+	private JTable table_1;
 	
-	public Admin() {
-			
-		setBackground(new Color(72, 209, 204));
-				
 		
+	public DefaultTableModel getModel() {
+		return model;
+	}
+
+
+	public void setModel(DefaultTableModel model) {
+		this.model = model;
+	}
+	
+
+	public void updateTable(){
+		
+		int c;
+		
+		PreparedStatement pStatement;
+		Connection conn = null;
+		ResultSet resultSet;
+		
+		try {
+			conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/librarydatabase", "root", "Connection");
+			pStatement = conn.prepareStatement("SELECT * FROM book");
+			
+			ResultSet result_set = pStatement.executeQuery();
+			ResultSetMetaData rs_metadata = result_set.getMetaData();
+			
+			c = rs_metadata.getColumnCount();
+			
+			DefaultTableModel df_model = (DefaultTableModel) table_1.getModel();
+			df_model.setRowCount(0);
+			
+				
+			while(result_set.next()){
+				Vector vector = new Vector<>();
+				
+				for(int index = 1; index <= c; index++){
+					vector.add(result_set.getString("bookID"));
+					vector.add(result_set.getString("author"));
+					vector.add(result_set.getString("title"));
+					vector.add(result_set.getString("subject"));
+					vector.add(result_set.getString("publisher"));
+					vector.add(result_set.getString("language"));
+				}
+				
+				df_model.addRow(vector);
+			}
+
+			author_TextField.requestFocus();
+			conn.close();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Error: please try again");
+		}
+	}
+	
+		
+	public Admin() {
+		
+					
+		setBackground(new Color(72, 209, 204));		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1186, 728);
 		contentPane = new JPanel();
@@ -69,30 +122,29 @@ public class Admin extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		table = new JTable();
+		//table = new JTable();
 		JScrollPane scrollPane = new JScrollPane();
+
+		
 		scrollPane.setBounds(440, 38, 716, 530);
 		contentPane.add(scrollPane);
 		
-		table = new JTable();
-		table.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		table.setBackground(new Color(220, 220, 220));
+		table_1 = new JTable();
+		table_1.setFont(new Font("Dialog", Font.PLAIN, 15));
+		table_1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table_1.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		table_1.setBackground(new Color(220, 220, 220));
 		model = new DefaultTableModel();
-		Object[] column_headers = {"Author", "Title", "Subject", "Publisher", "Language"};
+		Object[] column_headers = {"BookID", "Author", "Title", "Subject", "Publisher", "Language"};
 		model.setColumnIdentifiers(column_headers);
-		table.setModel(model);
-		scrollPane.setViewportView(table);
+		table_1.setModel(model);
+		scrollPane.setViewportView(table_1);
 				
 		JButton search_Button = new JButton("SEARCH");
 		search_Button.setBounds(12, 605, 149, 25);
 		search_Button.setFont(new Font("Georgia", Font.BOLD, 20));
 		contentPane.add(search_Button);
-		
-		JButton update_Button = new JButton("UPDATE ");
-		update_Button.setBounds(721, 605, 149, 25);
-		update_Button.setFont(new Font("Georgia", Font.BOLD, 20));
-		contentPane.add(update_Button);
-		
+			
 		JButton delete_Button = new JButton("DELETE ");
 		delete_Button.setBounds(979, 605, 149, 25);
 		delete_Button.setFont(new Font("Georgia", Font.BOLD, 20));
@@ -146,8 +198,7 @@ public class Admin extends JFrame {
 		language_ComboBox.setModel(language_model);
 		language_ComboBox.setBounds(163, 499, 246, 22);
 		contentPane.add(language_ComboBox);
-		
-		
+			
 		title_TextField = new JTextField();
 		title_TextField.setBounds(161, 133, 248, 22);
 		contentPane.add(title_TextField);
@@ -172,21 +223,53 @@ public class Admin extends JFrame {
 		contentPane.add(author_TextField);
 		author_TextField.setColumns(10);
 		
-		//ADD BUTTON:  ADD DATA TO DATABASE BUTTON
+		table_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+				String authorClicked = table_1.getValueAt(table_1.getSelectedRow(), 1).toString();
+				String titleClicked = table_1.getValueAt(table_1.getSelectedRow(), 2).toString();
+				String subjectClicked = table_1.getValueAt(table_1.getSelectedRow(), 3).toString();
+				String publisherClicked = table_1.getValueAt(table_1.getSelectedRow(), 4).toString();
+				String languageClicked = table_1.getValueAt(table_1.getSelectedRow(), 5).toString();
+				
+				author_TextField.setText(authorClicked);
+				title_TextField.setText(titleClicked);
+				subject_ComboBox.setSelectedItem(subjectClicked);
+				publisher_ComboBox.setSelectedItem(publisherClicked);
+				language_ComboBox.setSelectedItem(languageClicked);
+								
+			}
+		});
+		
+
+		
+		//ADD BUTTON:  ADD DATA TO DATABASE BUTTON AND DISPLAY ON TABLE
 				JButton add_Button = new JButton("ADD");
+				final Object[] add_row = new Object[6];
 				add_Button.addActionListener(new ActionListener() {			
 					public void actionPerformed(ActionEvent event) {
-						Connection connection = null;			
+						Connection connection = null;	
+						PreparedStatement statement;
+			
 						String aut = author_TextField.getText();
 						String tit = title_TextField.getText();
 						String sub = subject_ComboBox.getSelectedItem().toString();
 						String pub = publisher_ComboBox.getSelectedItem().toString();
 						String lang = language_ComboBox.getSelectedItem().toString();
-						
+												
+						add_row[1] = author_TextField.getText();
+						add_row[2] = title_TextField.getText();
+						add_row[3] = subject_ComboBox.getSelectedItem().toString();
+						add_row[4] = publisher_ComboBox.getSelectedItem().toString();
+						add_row[5] = language_ComboBox.getSelectedItem().toString();
+										
+						model.addRow(add_row);
+												
 						try {
 							connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/librarydatabase", "root", "Connection");
 							String sql = "INSERT INTO book (author, title, subject, publisher, language) VALUES (?, ?, ?, ?, ?)";
-							PreparedStatement statement = (PreparedStatement) connection.prepareStatement(sql);
+							statement = (PreparedStatement) connection.prepareStatement(sql);
 							statement.setString(1, aut);
 							statement.setString(2, tit);				
 							statement.setString(3, sub);
@@ -194,10 +277,12 @@ public class Admin extends JFrame {
 							statement.setString(5, lang);
 							
 							statement.executeUpdate();
-							JOptionPane.showMessageDialog(null, "The book has been added successfully");		
+							JOptionPane.showMessageDialog(null, "The book has been added successfully");
+							author_TextField.requestFocus();
+							updateTable();
 							connection.close();
 						} catch (SQLException e) {
-							JOptionPane.showMessageDialog(null, e);
+							JOptionPane.showMessageDialog(null, "Error: please try again");
 						}
 						
 					}
@@ -205,6 +290,51 @@ public class Admin extends JFrame {
 				add_Button.setBounds(470, 605, 149, 25);
 				add_Button.setFont(new Font("Georgia", Font.BOLD, 20));
 				contentPane.add(add_Button);
+								
+				//UPDATE BUTTON - UPDATES SELECTED RECORD
+				JButton update_Button = new JButton("UPDATE ");
+				update_Button.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						Connection connection = null;	
+						
+//						String aut = author_TextField.getText();
+//						String tit = title_TextField.getText();
+//						String sub = subject_ComboBox.getSelectedItem().toString();
+//						String pub = publisher_ComboBox.getSelectedItem().toString();
+//						String lang = language_ComboBox.getSelectedItem().toString();
+						
+						int selectIndex = table_1.getSelectedRow();
+						int id = Integer.parseInt(model.getValueAt(selectIndex, 0).toString());
+						
+						try {
+								
+								connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/librarydatabase", "root", "Connection");	
+								connection.setAutoCommit(false);
+								String sql = "UPDATE book SET author = ?, title = ?, subject = ?, publisher = ?, language = ? WHERE bookID = ?";
+								PreparedStatement statement = (PreparedStatement) connection.prepareStatement(sql);
+								statement.setInt(6, id);							
+								statement.setString(1, author_TextField.getText());
+								statement.setString(2, title_TextField.getText());
+								statement.setString(3, subject_ComboBox.getSelectedItem().toString());
+								statement.setString(4, publisher_ComboBox.getSelectedItem().toString());
+								statement.setString(5, language_ComboBox.getSelectedItem().toString());
+								statement.executeUpdate();											
+								JOptionPane.showMessageDialog(null, "The book has been updated successfully");								
+								model.fireTableDataChanged();
+								connection.commit();
+								
+						
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}catch(Throwable e) 
+						{
+							e.printStackTrace();
+						}
+					}
+				});
+				update_Button.setBounds(721, 605, 149, 25);
+				update_Button.setFont(new Font("Georgia", Font.BOLD, 20));
+				contentPane.add(update_Button);
 				
 		// CLEAR BUTTON
 		JButton clear_Button = new JButton("CLEAR");
@@ -219,11 +349,10 @@ public class Admin extends JFrame {
 			}
 		});
 		clear_Button.setFont(new Font("Georgia", Font.BOLD, 20));
-		contentPane.add(clear_Button);
+		contentPane.add(clear_Button);	
 			
 	}
 	
-
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
